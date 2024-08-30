85fa965060203c5c8fd1bceaecde8871cb62ed6c
Devan <devan.xu@foxmail.com>
2024-08-30 16:13:59 +0800

    Config for Github Pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,15 +8,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://anyui.tech',
+  url: 'https://lvgl-designer.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'anyui', // Usually your GitHub org/user name.
-  projectName: 'anyui-homepage', // Usually your repo name.
+  organizationName: 'lvgl-designer', // Usually your GitHub org/user name.
+  projectName: 'lvgl-designer.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
